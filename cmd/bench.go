1c608fa498f0bc09295ab6fdf549ca9f94840e35
crazycs <crazycs520@gmail.com>
2021-06-08 15:25:16 +0800

    add rand-val
    Signed-off-by: crazycs <crazycs520@gmail.com>

--- cmd/bench.go
+++ cmd/bench.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/spf13/cobra"
+	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"sync/atomic"
 	"time"
@@ -15,8 +17,13 @@ type BenchSQL struct {
 	query  string
 	ignore bool
 
+	randMin int64
+	randMax int64
+
 	totalQPS int64
 }
+
+const randValueStr = "#rand-val"
 
 func (b *BenchSQL) Cmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -30,6 +37,8 @@ func (b *BenchSQL) Cmd() *cobra.Command {
 	//cmd.Flags().IntVar(&app.EstimateTableRows, "new-table-row", 0, "estimate need be split table rows")
 	cmd.Flags().StringVarP(&b.query, "sql", "", "", "bench sql statement")
 	cmd.Flags().BoolVarP(&b.ignore, "ignore", "", false, "should ignore error?")
+	cmd.Flags().Int64VarP(&b.randMin, "randmin", "", 0, randValueStr +" min val")
+	cmd.Flags().Int64VarP(&b.randMax, "randmax", "", 0, randValueStr + " max val")
 
 	return cmd
 }
@@ -49,7 +58,7 @@ func (b *BenchSQL) RunE(cmd *cobra.Command, args []string) error {
 		fmt.Printf("-----------[ help ]-----------\n")
 		return cmd.Help()
 	}
-	fmt.Printf("sql: %v\nconcurrency: %v\n", b.query, b.cfg.Concurrency)
+	fmt.Printf("sql: %v\nconcurrency: %v\n", replaceSQL(b.query,b.randMin,b.randMax), b.cfg.Concurrency)
 	for i := 0; i < b.cfg.Concurrency; i++ {
 		go b.benchSql()
 	}
@@ -68,6 +77,7 @@ func (b *BenchSQL) benchSql() {
 		var err error
 		var rows *sql.Rows
 		for i := 0; i < batch; i++ {
+			sqlStr = replaceSQL(sqlStr,b.randMin,b.randMax)
 			if strings.HasPrefix(strings.ToLower(sqlStr), "select") {
 				rows, err = db.Query(sqlStr)
 			} else {
@@ -85,4 +95,15 @@ func (b *BenchSQL) benchSql() {
 		}
 		atomic.AddInt64(&b.totalQPS, int64(batch))
 	}
+}
+
+func replaceSQL(sql string,randMin,randMax int64) string {
+	if randMin == randMax {
+		return sql
+	}
+	if !strings.Contains(sql, randValueStr) {
+		return sql
+	}
+	v := rand.Intn(int(randMax-randMin+1)) + int(randMin)
+	return strings.Replace(sql,randValueStr,strconv.Itoa(v),-1)
 }
